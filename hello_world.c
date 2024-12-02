e850ab926a87cfebae25f8df1ca9b48d73bc60be
leemhuis1 <leemhuis@uni-wuppertal.de>
2024-12-02 11:18:57 +0100

    Zwischenstand: 4x1 Proc-grid, läuft noch nicht ganz.

--- hello_world.c
+++ hello_world.c
@@ -27,7 +27,7 @@ int main(int argc, char** argv){
 
 	int n = 4;       // (Global) Matrix size
 
-	int nprow = 1;   // Number of proc - rows
+	int nprow = 4;   // Number of proc - rows
 	int npcol = 1;   // Number of proc - cols
 //	char uplo='L';   // Matrix is lower triangular
 	char layout='R'; // Block cyclic, Row major processor mapping
@@ -56,7 +56,7 @@ int main(int argc, char** argv){
  *		0.9691
 */
 
-	double *A = malloc( 16 *sizeof(double));
+	double *A = malloc( 4 *sizeof(double));
 	double *B = malloc( 4 *sizeof(double));
 	double *X = malloc( 4 *sizeof(double));
 
@@ -67,7 +67,7 @@ int main(int argc, char** argv){
 		A[1] = 75;
 		A[2] = 146;
 		A[3] = 76;
-		
+		/*
 		A[4] = 75;
 		A[5] = 10108;
 		A[6] = 132;
@@ -82,7 +82,7 @@ int main(int argc, char** argv){
 		A[13] = 90;
 		A[14] = 138;
 		A[15] = 10086;
-
+*/
 
 		B[0] = 5;
 		B[1] = 10;
@@ -101,28 +101,32 @@ int main(int argc, char** argv){
 
 		B[0] = 5;
 		B[1] = 10;
-		X[0] = 0.4695e-3;
-		X[1] = 0.9674e-3;
+		B[2] = 8;
+		B[3] = 10;		
+		X[0] = 0.9674e-3;
 	} else if (process_rank == 2){
 		A[0] = 146;
 		A[1] = 132;
 		A[2] = 76;
 		A[3] = 90;
 
-		B[0] = 8;
+		B[0] = 5;
 		B[1] = 10;
+		B[2] = 8;
+		B[3] = 10;		
 		X[0] = 0.7471e-3;
-		X[1] = 0.9691e-3;
 	} else if (process_rank == 3){
 		A[0] = 10266;
 		A[1] = 138;
 		A[2] = 138;
 		A[3] = 10086;
 
-		B[0] = 8;
-		B[1] = 10;		
-		X[0] = 0.7471e-3;
-		X[1] = 0.9691e-3;
+
+		B[0] = 5;
+		B[1] = 10;
+		B[2] = 8;
+		B[3] = 10;		
+		X[0] = 0.9691e-3;
 	}    
 
 	MPI_Barrier(MPI_COMM_WORLD);
@@ -145,7 +149,7 @@ int main(int argc, char** argv){
 
     // Compute the size of the local matrices
 	int bsx = 4; //blocksize in X direction
-	int bsy = 4; //blocksize in Y direction
+	int bsy = 1; //blocksize in Y direction
 	
 	int numc = numroc_( &n, &bsx, &mycol, &zero, &npcol ); // number of columns stored in each process
 	int numr = numroc_( &n, &bsy, &myrow, &zero, &nprow ); // number of rows stored in each process
@@ -161,8 +165,8 @@ int main(int argc, char** argv){
 	int info = 0;
 	int lddA = numc > 1? numc : 1;
 
-	int bfc = n / numc; //blocking factor columns 	// = 1 ()
-	int bfr = n / numr; //blocking factor rows 	// = 4 ()
+	int bfc = n / numc; //blocking factor columns 	// = 1 ( = bsy)
+	int bfr = n / numr; //blocking factor rows 	// = 4 ( = bsx)
 		
 	MPI_Barrier(MPI_COMM_WORLD);
 	for (i = 0; i < 4; i++){
@@ -185,24 +189,25 @@ int main(int argc, char** argv){
 	
     // Compute the size of the local matrices
 	int bsxB = 1; //blocksize in X direction
-	int bsyB = 4; //blocksize in Y direction
+	int bsyB = 1; //blocksize in Y direction
 	
-	int numcB = numroc_( &n, &bsxB, &mycol, &zero, &npcol ); // number of columns stored in each process
+	int numcB = numroc_( &one, &bsxB, &mycol, &zero, &npcol ); // number of columns stored in each process
 	int numrB = numroc_( &n, &bsyB, &myrow, &zero, &nprow ); // number of rows stored in each process
-	numcB = 1;
 	int descB[9];
-	int lddB = numrB > 1? numrB : 1; 	//number of local rows in b;
+	int lddB = numcB > 1? numcB : 1; 	//number of local rows in b;
 	
 
 	int bfrB = n / numrB;	//blocking factor - col (blocksize in cols)
-	int bfcB = n / numcB;	//blocking factor - row (blocksize in rows)
-	
+	int bfcB = 1 / numcB;	//blocking factor - row (blocksize in rows)
+
+	bfrB = bsxB;
+	bfcB = bsyB;
+
 	MPI_Barrier(MPI_COMM_WORLD);
 	for (i = 0; i < 4; i++){
 		if (process_rank == i) printf("I am process %d, \tnumrB = %d, numcB = %d,\tbfrB = %d, bfcB = %d, \tlddB = %d\n", process_rank, numrB, numcB, bfrB, bfcB, lddB);
 		MPI_Barrier(MPI_COMM_WORLD);
 	}
-//DESCB =            1           0           4           1           1           4           0           0           4
 //DESCB =            1           0           4           1           4           1           0           0           4
 	descinit_( descB, &n, &one, &bfrB, &bfcB, &zero, &zero, &ictxt, &lddB, &info);
 	if(info != 0) {
@@ -223,10 +228,10 @@ int main(int argc, char** argv){
 	t0 = MPI_Wtime();
 
 	int nrhs = numcB;	//1;
-	int ia = (myrow +1)/ 2 +1;
-	int ib = (myrow +1)/ 2 +1;
-	int ja = mycol % 2 +1; // = 0;
-	int jb = 0 +1;	// = 0;
+	int ia = myrow +1;
+	int ib = myrow +1;
+	int ja = 1; // = 0;
+	int jb = 1;	// = 0;
 	int ipiv[4];
 	for (i = 0; i<4; i++) ipiv[i] = i;
 
@@ -238,7 +243,6 @@ int main(int argc, char** argv){
 		MPI_Barrier(MPI_COMM_WORLD);
 	}
 
-	n = 4;
 	//PDGESV( N, NRHS, A, IA, JA, DESCA, IPIV, B, IB, JB, DESCB, INFO )
 	if (process_rank == 0) printf("n = %d, ipiv: [%d, %d, %d, %d]\n", n, ipiv[0], ipiv[1], ipiv[2], ipiv[3]);
 	pdgesp_( &n, &one, A, &ia, &ja, descA, &zero, B, &ib, &jb, descB, &info);
